67a93080897fe4fcb6f4c571320088d3ada8b135
HenryThompson0 <sayjokesphotos@gmail.com>
2024-10-31 09:54:23 -0400

    Update GuessingGame.java

--- GuessingGame.java
+++ GuessingGame.java
@@ -8,14 +8,37 @@ import static java.lang.System.*;
 public class GuessingGame
 {
 	private int upperBound;
+    private int compChoice;
+    private int playChoice;
+    private double numGuesses;
+    private double percent;
 
 	public GuessingGame(int stop)
 	{
+        upperBound = stop;
+        compChoice = (int)(Math.random()*upperBound+1);
+        playGame();
 	}
 
 	public void playGame()
 	{
-		Scanner keyboard = new Scanner(System.in);
+        do
+        {
+            Scanner keyboard = new Scanner(System.in);
+            System.out.print("Guess a number between 1 and "+upperBound+": ");
+            playChoice = keyboard.nextInt();
+            if (playChoice>upperBound || playChoice<1)
+            {
+                System.out.println("That number is out of range! ");
+            }
+            numGuesses += 1;
+
+        } while (playChoice != compChoice);
+        
+        percent = ((numGuesses-1)/numGuesses)*100;
+
+        System.out.println("It took you "+(int)numGuesses+" guesses to guess "+compChoice+". ");
+        System.out.println("You guessed wrong "+(int)percent+" percent of the time.");
 	}
 
 	public String toString()
