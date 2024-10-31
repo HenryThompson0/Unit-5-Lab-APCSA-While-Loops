2264fad3a4f1421c457d2861ebed281ef1967b55
HenryThompson0 <sayjokesphotos@gmail.com>
2024-10-31 09:15:47 -0400

    Update DivisorsRunner.java

--- DivisorsRunner.java
+++ DivisorsRunner.java
@@ -6,6 +6,12 @@ public class DivisorsRunner
 {
 	public static void main( String args[] )
 	{
-		//add test cases 
+        int[] cases = {10,45,14,1024,1245,33,65535};
+        Divisors n = new Divisors();
+
+        for(int val : cases)
+        {
+            System.out.print(n.getDivisors(val));
+        }
 	}
 }
