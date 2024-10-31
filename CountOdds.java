af08599011f0cc88e2e879190b1d887aae18ea7d
HenryThompson0 <sayjokesphotos@gmail.com>
2024-10-31 09:16:06 -0400

    Update CountOdds.java

--- CountOdds.java
+++ CountOdds.java
@@ -6,6 +6,24 @@ public class CountOdds
 {
    public static int go( int number )
 	{
-		return 0;
+        String numDigitsPlaceholder = (String.valueOf(number));
+        int numDigits = numDigitsPlaceholder.length();
+
+        int numOddDigits = 0;
+
+        while (numDigits>0)
+        {
+            int singleDigit = number%10;
+            number = number/10;
+
+            if (singleDigit % 2 != 0)
+            {
+                numOddDigits += 1;
+            }
+
+            numDigits -= 1;
+        }
+
+		return numOddDigits;
 	}
 }
