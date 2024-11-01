712fde0f638547b68bd1bedfc241b51581b2f181
HenryThompson0 <sayjokesphotos@gmail.com>
2024-11-01 11:05:39 -0400

    Update PassWordCheck.java

--- PassWordCheck.java
+++ PassWordCheck.java
@@ -7,14 +7,34 @@ import static java.lang.System.*;
 
 class PasswordCheck
 {
-	private String password;
-
-	public PasswordCheck()
-	{
-	}
+	private String password = "crazy123";
 
 	public void check()
 	{
 		Scanner keyboard = new Scanner(System.in);
+        System.out.print("Enter your Password: ");
+        String passwordChoice = keyboard.nextLine();
+
+        PasswordCheck check = new PasswordCheck(passwordChoice);
+        check();
+	}
+
+    public PasswordCheck(String passwordChoice)
+	{
+        if (passwordChoice==null)
+        {
+            check();
+        }
+
+        if (password.equals(passwordChoice))
+        {
+            System.out.println("VALID");
+            System.exit(0);
+        }
+
+        else
+        {
+            System.out.println("INVALID");
+        }
 	}
 }
